4e48349d181dc1ecef0df09409dddfc545bfae25
dancost <ddan.costache@gmail.com>
2024-07-21 17:49:38 +0300

    extract base_url from env var

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 from api.AppAPI import ForexAPI
+import os
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
 
 
 @pytest.fixture(scope="session")
 def forex_api_session():
+    print(f"USING BASE URL: {BASE_URL}")
     api_client = ForexAPI(base_url=BASE_URL)
     return api_client
