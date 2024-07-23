39df0c0c4fe9999363a691d48e33e71fd40c7431
dancost <ddan.costache@gmail.com>
2024-07-23 13:47:18 +0300

    add some comments

--- websocket_manager.py
+++ websocket_manager.py
@@ -2,26 +2,27 @@
 import json
 from collections import defaultdict
 from fastapi import WebSocket
-from typing import Set
+from typing import Set, Dict
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class ConnectionManager:
-    # store active ws connections and subscribers
     def __init__(self):
+        # initialize and store all active connections
         self.active_connections: Set[WebSocket] = set()
-        self.order_subscribers: defaultdict[str, Set[WebSocket]] = defaultdict(set)
+        # store subscribers and order id
+        self.order_subscribers: Dict[str, Set[WebSocket]] = defaultdict(set)
 
     async def connect(self, websocket: WebSocket):
-        # add connection and add it to active ws connections
+        # accept new connection and add it to active connections
         await websocket.accept()
         self.active_connections.add(websocket)
         logger.info("WebSocket connected")
 
     def disconnect(self, websocket: WebSocket):
-        # remove ws connection from active connections and any subscription
+        # remove websockets connection from active connections and subscriptions
         self.active_connections.remove(websocket)
         for subscribers in self.order_subscribers.values():
             if websocket in subscribers:
@@ -29,11 +30,10 @@ class ConnectionManager:
         logger.info("WebSocket disconnected")
 
     async def broadcast(self, message: dict, order_id: str = None):
-        # if an order ID is provided, send the message only to subscribed clients for that order
+        # send messages to subscribed clients
         if order_id:
             subscribers = self.order_subscribers[order_id]
-            logger.info(
-                f"Broadcasting message to {len(subscribers)} subscriber(s) for order ID: {order_id}")
+            logger.info(f"Broadcasting message to {len(subscribers)} subscriber(s) for order ID: {order_id}")
             for connection in subscribers:
                 await connection.send_text(json.dumps(message))
         else:
