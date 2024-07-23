2d6b1fa1ff2e6bc3f8288fecf458f4b08fe8a35e
dancost <ddan.costache@gmail.com>
2024-07-23 11:50:15 +0300

    move logic to load test sample to conftest.py for reuse in all required tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from api.AppAPI import ForexAPI
 import os
+import json
 
 BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
 
@@ -10,3 +11,12 @@ def forex_api_session():
     print(f"USING BASE URL: {BASE_URL}")
     api_client = ForexAPI(base_url=BASE_URL)
     return api_client
+
+
+@pytest.fixture(scope='function')
+def load_sample_order():
+    # load the test sample and pass it to test functions
+    json_file_path = 'test_data/new_order_sample.json'
+    with open(json_file_path, 'r') as file:
+        sample_order = json.load(file)
+    return sample_order
